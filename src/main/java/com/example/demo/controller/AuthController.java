e423144bab4d7612e4c98b098551d26fd13aee35
Roy.Xu <bbb233456@gmail.com>
2026-07-03 04:07:23 +0800

    [Feature] Add /api/register for guest registration (Issue: #4)

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.dto.RegistryDTO;
+import com.example.demo.service.UserService;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void register(@RequestBody RegistryDTO request) {
+        userService.register(request);
+    }
+}
